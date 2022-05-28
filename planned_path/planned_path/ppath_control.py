2748536ca96eeddc4db07e236001fe2901c90744
ggao22 <g.gao@wustl.edu>
2022-05-28 14:23:01 -0500

    simple path

--- planned_path/planned_path/ppath_control.py
+++ planned_path/planned_path/ppath_control.py
@@ -4,33 +4,40 @@ from rclpy.node import Node
 from ackermann_msgs.msg import AckermannDriveStamped
 
 class PlannedPath(Node):
+
     def __init__(self):
         super().__init__("planned_path")
         self.get_logger().info("Started planned path...")
 
-        self._pulisher = self.create_publisher(AckermannDriveStamped, "drive", 1)
+        self._publisher = self.create_publisher(AckermannDriveStamped, "drive", 1)
+        self.period_output = self.create_timer(0.5,self.drive)
     
     def drive(self):
         msg = AckermannDriveStamped()
-        
-        msg.header.stamp.sec = 10.0
-        msg.header.stamp.nanosec = 0.0
-        msg.header.stamp.frame_id = "planned_path"
-        msg.drive.steering_angle = 0
-        msg.drive.steering_angle_velocity = 0
-        msg.drive.speed = 1
-        msg.drive.acceleration = 0
-        msg.drive.jerk = 0
+
+        msg.header.stamp.sec = 10
+        msg.header.stamp.nanosec = 0
+        msg.header.frame_id = "planned_path"
+        msg.drive.steering_angle = 0.0
+        msg.drive.steering_angle_velocity = 0.0
+        msg.drive.speed = 1.0
+        msg.drive.acceleration = 0.0
+        msg.drive.jerk = 0.0
 
         self._publisher.publish(msg)
+        self.get_logger().info('Publishing: planned path...')
         
 
-def main():
-    rclpy.init(args=args)
-    node = PlannedPath()
-    rclpy.spin(node)
-    rclpy.shutdown()
+def main(args=None):
+    try:
+        rclpy.init(args=args)
+        path_publisher = PlannedPath()
+        rclpy.spin(path_publisher)
+        path_publisher.destroy_node()
+        rclpy.shutdown()
+    except KeyboardInterrupt:
+        pass
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
+
