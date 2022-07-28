772ce2d642a4905c4362545cf5575440bcfde312
yvxavier <you@example.com>
2022-07-27 23:54:44 -0500

    init lanenet driver

---
+++ lanenet_driver/lanenet_driver/lanenet_driver_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from lanenet_driver.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ lanenet_driver/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'lanenet_driver'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='yvxaiver',
+    maintainer_email='you@example.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'lanenet_driver_node = lanenet_driver.lanenet_driver_node:main'
+        ],
+    },
+)
